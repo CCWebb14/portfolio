bef1290242c1b20b122287267926d70b5a4745d3
CCWebb14 <CCWebb14@gmail.com>
2022-11-02 09:33:58 -0700

    added home to the components folder and adjusted imports to master index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css';
-import Home from './Home';
+import Home from './components/Home';
 import NavTabs from './components/NavigationBar';
 import Footer from './components/Footer';
 
