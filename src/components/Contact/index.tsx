4021ade8f68556348bdd6b741a5371d09f644219
CCWebb14 <CCWebb14@gmail.com>
2022-11-02 15:57:09 -0700

    Contact page completed

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material';
 
-const Contact = () => <></>;
+const Contact = () => (
+  <>
+    <Box
+      sx={{
+        display: 'flex',
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Card
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: 350,
+          height: 200,
+        }}
+      >
+        <Typography>
+          Cooper Webb <br />
+          Victoria, B.C.
+          <br />
+          CCWebb14@gmail.com
+        </Typography>
+      </Card>
+    </Box>
+  </>
+);
 
 export default Contact;
