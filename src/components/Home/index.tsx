87e62e00a9c4812c2dc980cbf6cfaff374cbbf42
CCWebb14 <CCWebb14@gmail.com>
2022-11-03 14:34:59 -0700

    added additional box to home with black background, fade in from black instead of white

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -20,50 +20,52 @@ const StyledBox = styled(Box)({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  backgroundImage: `url(${background})`,
+  background: `url(${background})`,
   backgroundSize: 'cover',
   height: '100%',
   width: '100%',
   backgroundPosition: 'center center',
   backgroundRepeat: 'no-repeat',
-  animation: `${fadeIn} 1s`,
+  animation: `${fadeIn} 2s`,
 });
 
 function Home() {
   return (
-    <StyledBox>
-      <Typography
-        sx={{
-          fontSize: 60,
-          marginTop: '5vh',
-          '@media(max-width: 480px)': { fontSize: 40 },
-        }}
-        color="white"
-        fontFamily="Montserrat"
-        fontWeight="500"
-      >
-        {"Hi, I'm Cooper"}
-      </Typography>
-      <Typography
-        sx={{
-          marginBottom: '2vh',
-          fontSize: 30,
-        }}
-        color="#0ad43c"
-        fontFamily="Montserrat"
-        fontWeight="300"
-      >
-        {'a web developer'}
-      </Typography>
-      <Button
-        variant="outlined"
-        color="success"
-        to="/projects"
-        component={Link}
-      >
-        {"Come see what I've been working on"}
-      </Button>
-    </StyledBox>
+    <Box sx={{ flexGrow: '1', background: '#000000' }}>
+      <StyledBox>
+        <Typography
+          sx={{
+            fontSize: 60,
+            marginTop: '5vh',
+            '@media(max-width: 480px)': { fontSize: 40 },
+          }}
+          color="white"
+          fontFamily="Montserrat"
+          fontWeight="500"
+        >
+          {"Hi, I'm Cooper"}
+        </Typography>
+        <Typography
+          sx={{
+            marginBottom: '2vh',
+            fontSize: 30,
+          }}
+          color="#0ad43c"
+          fontFamily="Montserrat"
+          fontWeight="300"
+        >
+          {'a web developer'}
+        </Typography>
+        <Button
+          variant="outlined"
+          color="success"
+          to="/projects"
+          component={Link}
+        >
+          {"Come see what I've been working on"}
+        </Button>
+      </StyledBox>
+    </Box>
   );
 }
 
